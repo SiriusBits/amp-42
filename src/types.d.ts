04e4f3aa5f8855c891931140312ee4336a5f3443
Benjamin Bykowski <benjamin.bykowski@gmail.com>
2024-01-03 15:18:47 -0500

    Applies updates from Astrowind

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,5 @@
 import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
-import type { HTMLAttributes } from 'astro/types';
+import type { HTMLAttributes, ImageMetadata } from 'astro/types';
 export interface Post {
   /** A unique ID number that identifies a post. */
   id: string;
@@ -20,7 +20,7 @@ export interface Post {
   /** Optional summary of post content. */
   excerpt?: string;
   /**  */
-  image?: string;
+  image?: ImageMetadata | string;
 
   /**  */
   category?: string;
@@ -196,6 +196,7 @@ export interface CallToAction extends HTMLAttributes<a> {
   text?: string;
   icon?: string;
   classes?: Record<string, string>;
+  type?: 'button' | 'submit' | 'reset';
 }
 
 export interface ItemGrid {
